26689c75148119fa4850085a15c9c0a0e0504c21
Håvard Pettersen <havpet@protonmail.ch>
2025-06-25 11:34:30 +0200

    Initial file add

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ipvx
-IP lookup tool with threat intelligence through 3rd party providers
+IP lookup tool in PHP with threat intelligence features.
+
+Features:
+* GET / : IP in plain text
+* GET /host : IP and host in plain text
+* GET /json : IP and host in JSON
+* GET /9.9.9.9 : Info about specific IP in JSON (configured to use ipinfo.io and AbuseIPDB)
+
+## Usage:
+1. Switch out **$ipinfotoken** with your token from https://ipinfo.io/developers and **$abuseipdbtoken** with your token from https://abuseipdb.com/
+2. Host however you like
+3. Remember to follow the IP lookup site's ToS
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+
+    if(empty($_SERVER['HTTPS'])) {
+        header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    }
+
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Origin: *");
+    header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $input = $_GET['input'];
+
+    $ipinfotoken = ""; // Your ipinfo.io API token
+    $abuseipdbtoken = ""; // Your abuseipdb token
+
+    $allowed_ip = in_array($_SERVER['REMOTE_ADDR'], ['0.0.0.0']); // Whitelisted IP's for the IP API calls
+
+    // standard ip return
+    if (!isset($input) || $input == "index.php") {
+        header('Content-Type: text/plain;');
+        echo $ip;
+    }
+
+    else {
+        // /host
+        if($input == "host") {
+            header('Content-Type: text/plain;');
+            echo $ip . ' | ' . gethostbyaddr($ip);
+        }
+
+        // /json
+        else if($input == "json") {
+            header('Content-Type: application/json; charset=utf-8');
+
+            $ip_array = Array (
+                "ip" => $ip,
+                "hostname" => gethostbyaddr($ip)
+            );
+
+            echo json_encode($ip_array, JSON_PRETTY_PRINT);
+        }
+
+        // /x.x.x.x
+        else if($allowed_ip && filter_var($input, FILTER_VALIDATE_IP,FILTER_FLAG_IPV4) || filter_var($input, FILTER_VALIDATE_IP,FILTER_FLAG_IPV6)) {
+            header('Content-Type: application/json; charset=utf-8');
+
+            $ip_details = json_decode(file_get_contents("https://ipinfo.io/{$input}/json?token={$ipinfotoken}"));
+            
+            $abuse_ip_details = json_decode(file_get_contents(
+                "https://api.abuseipdb.com/api/v2/check?ipAddress={$input}",
+                false,
+                stream_context_create([
+                    'http' => [
+                        'method' => 'GET',
+                        'header' => "Key: {$abuseipdbtoken}"
+                    ]
+                ])
+            ));
+
+            $ip_array = Array (
+                "ip" => $input,
+                "hostname" => $ip_details->hostname,
+                "city" => $ip_details->city,
+                "region" => $ip_details->region,
+                "country" => $ip_details->country,
+                "org" => $ip_details->org,
+                "abuse_confidence" => $abuse_ip_details->data->abuseConfidenceScore,
+                "num_abuse_reports" => $abuse_ip_details->data->totalReports,
+                "abuse_info" => "https://www.abuseipdb.com/check/{$input}"
+            );
+
+            echo json_encode($ip_array, JSON_PRETTY_PRINT);
+        }
+
+        else {
+            header('Location: /');
+        }
+    }
+    
+?>
